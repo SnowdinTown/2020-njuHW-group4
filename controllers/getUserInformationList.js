ad9c1853df56bde1d0d755d60aa47ecf7bbe5484
Gabri <1034688982@qq.com>
2020-06-21 18:13:51 +0800

    fix: check activity time

--- controllers/getUserInformationList.js
+++ controllers/getUserInformationList.js
@@ -1,8 +1,10 @@
 const mysql=require('../middleware/mysql')
+const moment=require('moment')
 
 module.exports=async (ctx)=>{
     const open_id=ctx.query.open_id;
     //const open_id="o4chG42YnTIfb9fQYQ5LidjXS76Y";
+    //const open_id="o4chG412hg0qJLpdXyTNOAW4G3mk";
     const information=await mysql('user_ticket')
         .join('activity','user_ticket.activity_id','activity.activity_id')
         .join('association','activity.association_id','association.id')
@@ -11,8 +13,11 @@ module.exports=async (ctx)=>{
     var res=[];
     for(var i=0;i<information.length;i++){
         var ifm=information[i];
-        var date=JSON.stringify(ifm.date).substr(1,10);
-        console.log(date)
+        var activity_time=moment(JSON.stringify(ifm.date).substr(1,10)+' '+ifm.time).add(1,'days')
+        var cur_time=moment()
+        var dis=cur_time.diff(activity_time,"hour")
+        if(dis<-24||dis>=0)continue
+        var date=JSON.stringify(moment(ifm.date).add(1,'days')).substr(1,10);
         var cur={
             time:date.substr(0,4)+"年"+date.substr(5,2)+"月"+date.substr(8,2)+"日"+" "+ifm.time.substr(0,5),
             image_src:ifm.image_src,
