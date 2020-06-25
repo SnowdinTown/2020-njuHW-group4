8f9b75adfc248f43dbb7fa1c66f5687c49ddd460
Y <1478928475@qq.com>
2020-06-25 18:40:35 +0800

    feat: 更改投票

--- controllers/voteActivity/addAnswer.js
+++ controllers/voteActivity/addAnswer.js
@@ -2,25 +2,29 @@ const mysql = require('../../middleware/mysql');
 
 
 module.exports = async (ctx) =>{
-    const open_id=ctx.request.body.open_id,
-        option_id=ctx.request.body.option_id,
-        content=ctx.request.body.content;
+    const answers=eval('('+ctx.request.body.answers+')');
     try {
-        await mysql('answer')
-            .insert({
-                open_id:open_id,
-                option_id:option_id,
-                content:content
-            })
-        var option_chosen=await mysql('option')
-            .select('option.*')
-            .where('option_id',option_id);
-        var num=option_chosen[0].chosen_num+1;
-        await mysql('option')
-            .where('option_id',option_id)
-            .update({
-                chosen_num:num
-            });
+        for(var i=0;i<answers.length;i++) {
+            var answer=answers[i];
+            var open_id=answer.open_id;
+            var option_id=answer.option_id;
+            var content=answer.content;
+            await mysql('answer')
+                .insert({
+                    open_id: open_id,
+                    option_id: option_id,
+                    content: content
+                })
+            var option_chosen = await mysql('option')
+                .select('option.*')
+                .where('option_id', option_id);
+            var num = option_chosen[0].chosen_num + 1;
+            await mysql('option')
+                .where('option_id', option_id)
+                .update({
+                    chosen_num: num
+                });
+        }
         ctx.response.status=200;
         var c={
             msg:'True'
--- controllers/voteActivity/getAllVotes.js
+++ controllers/voteActivity/getAllVotes.js
@@ -11,6 +11,9 @@ module.exports = async (ctx) =>{
         var options=await mysql('option')
             .select('option.*')
             .where('activity_id',vote_id);
+        var data1=await mysql('association')
+            .select('name')
+            .where('id',data[i].association_id);
         var cur={
             activity_id:data[i].activity_id,
             association_id:data[i].association_id,
@@ -20,7 +23,9 @@ module.exports = async (ctx) =>{
             vote_description:data[i].vote_description,
             vote_content:data[i].vote_content,
             vote_type:data[i].vote_type,
-            options:options
+            options:options,
+            association_name:data1[0].name
+
         }
         res.push(cur)
     }
