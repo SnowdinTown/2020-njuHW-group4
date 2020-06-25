935711e6db8b589fbf68cb285d8daf33178feab7
Snowding Zerland <181250011@smail.nju.edu.cn>
2020-06-25 16:54:41 +0800

    fix: 修改updateLikes的逻辑

--- controllers/messageWall/updateLikes.js
+++ controllers/messageWall/updateLikes.js
@@ -1,19 +1,22 @@
 const mysql = require('../../middleware/mysql');
 
 module.exports = async (ctx) => {
-    const message_id = ctx.request.body.message_id,
-        likes = eval(ctx.request.body.likes);
+    const message_id = ctx.request.body.message_id;
 
     try{
+        let likes = await mysql('message_wall')
+            .select('likes')
+            .where('message_id',message_id)
         await mysql('message_wall')
             .where('message_id',message_id)
-            .update({likes:likes});
+            .update({likes:likes[0].likes+1});
         ctx.response.status = 200;
     }catch (e) {
         ctx.status = 500;
-        ctx.state = {
+        ctx.body = {
             code: -1,
             msg: e.sqlMessage
         }
+        console.log(e)
     }
 }
