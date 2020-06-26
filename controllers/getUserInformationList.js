59d4ce93c4b53b89fc6991d82b163fdf944b8f15
Y <1478928475@qq.com>
2020-06-26 16:30:17 +0800

    feat: 活动通知更改

--- controllers/getUserInformationList.js
+++ controllers/getUserInformationList.js
@@ -8,7 +8,7 @@ module.exports=async (ctx)=>{
     const information=await mysql('association_joiner')
         .join('activity','association_joiner.association_id','activity.association_id')
         .join('association','activity.association_id','association.id')
-        .select('activity.date','activity.time','association.image_src','activity.activity_name','association.name')
+        .select('activity.activity_id','activity.date','activity.time','association.image_src','activity.activity_name','association.name')
         .where('association_joiner.open_id',open_id)
     var res=[];
     for(var i=0;i<information.length;i++){
@@ -22,7 +22,8 @@ module.exports=async (ctx)=>{
             time:date.substr(0,4)+"年"+date.substr(5,2)+"月"+date.substr(8,2)+"日"+" "+ifm.time.substr(0,5),
             image_src:ifm.image_src,
             information:ifm.activity_name+"活动将于24小时后开始",
-            name:ifm.name
+            name:ifm.name,
+            id:ifm.activity_id
         }
         res.push(cur)
     }
